ca518f0fe61cd25e5b6ba2d6784deaa21c459e65
yogatriardhana <44792017+yogatriardhana@users.noreply.github.com>
2020-01-22 16:35:37 +0700

    hmnn
    testing

---
+++ data/FASILITAS_5.js
@@ -0,0 +1,18 @@
+var json_FASILITAS_5 = {
+"type": "FeatureCollection",
+"name": "FASILITAS_5",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": null, "Objek": "Sekolah", "NAMA": null }, "geometry": { "type": "Point", "coordinates": [ 110.829161016467822, -7.022418995888524 ] } },
+{ "type": "Feature", "properties": { "id": null, "Objek": "Sekolah", "NAMA": null }, "geometry": { "type": "Point", "coordinates": [ 110.830587784149472, -7.025011521548736 ] } },
+{ "type": "Feature", "properties": { "id": null, "Objek": "Sekolah", "NAMA": null }, "geometry": { "type": "Point", "coordinates": [ 110.846008086665037, -7.031723065331129 ] } },
+{ "type": "Feature", "properties": { "id": null, "Objek": "Sekolah", "NAMA": null }, "geometry": { "type": "Point", "coordinates": [ 110.848964378496319, -7.032958824920548 ] } },
+{ "type": "Feature", "properties": { "id": null, "Objek": "Kantor Desa", "NAMA": null }, "geometry": { "type": "Point", "coordinates": [ 110.845007153178713, -7.029875862142696 ] } },
+{ "type": "Feature", "properties": { "id": null, "Objek": "Masjid", "NAMA": null }, "geometry": { "type": "Point", "coordinates": [ 110.829266295960977, -7.025889237913003 ] } },
+{ "type": "Feature", "properties": { "id": null, "Objek": "Makam", "NAMA": null }, "geometry": { "type": "Point", "coordinates": [ 110.831524342829724, -7.027912566106475 ] } },
+{ "type": "Feature", "properties": { "id": null, "Objek": "Masjid", "NAMA": null }, "geometry": { "type": "Point", "coordinates": [ 110.842902995235235, -7.029444046985493 ] } },
+{ "type": "Feature", "properties": { "id": null, "Objek": "Makam", "NAMA": null }, "geometry": { "type": "Point", "coordinates": [ 110.840575105878074, -7.028974889599638 ] } },
+{ "type": "Feature", "properties": { "id": null, "Objek": "Makam", "NAMA": null }, "geometry": { "type": "Point", "coordinates": [ 110.846925021546653, -7.029807301364152 ] } },
+{ "type": "Feature", "properties": { "id": null, "Objek": "Masjid", "NAMA": null }, "geometry": { "type": "Point", "coordinates": [ 110.85014149177222, -7.033704339226258 ] } }
+]
+}
